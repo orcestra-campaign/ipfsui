0df73bdfae3c7b7d82d4c773fcd3382050d82e04
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2025-01-22 18:12:17 +0100

    fixup

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  Deno.env.get("FIREBASE_API_KEY");
   return {
     plugins: [vue()],
     base: mode == "production" ? "/ipfsui/" : "/",
