619be9ef2684c3c7c0807bdb3aa92a7eb181520c
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2025-01-22 18:10:02 +0100

    set deployment prefix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  Deno.env.get("FIREBASE_API_KEY");
+  return {
+    plugins: [vue()],
+    base: mode == "production" ? "/ipfsui/" : "/",
+  };
 });
