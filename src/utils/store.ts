2bbc0eeb4145ab746bb3bc01e9f5978bb0e9a060
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2025-01-22 21:01:18 +0100

    deno fmt

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,13 +1,13 @@
-import IPFSFetchStore from './ipfs/fetchStore';
-import { FetchStore } from 'zarrita';
+import IPFSFetchStore from "./ipfs/fetchStore";
+import { FetchStore } from "zarrita";
 
 export function getStore(url: string | URL) {
-	const [protocol, _] = (typeof url === "string" ? url : url.href).split(
-		"://",
-	);
-	if (protocol === "ipfs" || protocol === "ipns") {
-		return new IPFSFetchStore(url);
-	} else {
-        return new FetchStore(url);
-    }
+  const [protocol, _] = (typeof url === "string" ? url : url.href).split(
+    "://",
+  );
+  if (protocol === "ipfs" || protocol === "ipns") {
+    return new IPFSFetchStore(url);
+  } else {
+    return new FetchStore(url);
+  }
 }
