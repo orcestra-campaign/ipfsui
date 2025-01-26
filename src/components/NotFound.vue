3f1243db3fd1e76101f223cf5bfd00bef42523f2
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2025-01-26 23:58:49 +0100

    add NotFound route

---
+++ src/components/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Page Not Found</h1>
+    If you intended to inspect a dataset, try navigating to <code>#/ds/&lt;your dataset url&gt;</code>
+</template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import DSContainer from "./components/DSContainer.vue";
 import DummyItem from "./components/DummyItem.vue";
+import NotFound from "./components/NotFound.vue";
 
 export const routes = [
   { path: "/", component: DummyItem },
   { path: "/ds/:src(.+)", component: DSContainer, props: true },
+  { path: "/:catchAll(.*)*", component: NotFound },
 ];
