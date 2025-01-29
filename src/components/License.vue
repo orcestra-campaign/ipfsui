1b1f17cfbbb06318e02c9b8dcb521171433c5d76
Lukas Kluft <lukas.kluft@gmail.com>
2025-01-29 12:12:25 +0100

    Add CC license logos

---
+++ src/components/License.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import { computed } from "vue";
+
+const {spdx} = defineProps<{ spdx: string }>();
+
+const licenseImage = computed(() =>
+  spdx.startsWith('CC-BY-NC-SA') ? '/src/images/by-nc-sa.svg' :
+  spdx.startsWith('CC-BY-NC') ? '/src/images/by-nc.svg' :
+  spdx.startsWith('CC-BY-SA') ? '/src/images/by-sa.svg' :
+  spdx.startsWith('CC-BY') ? '/src/images/by.svg' :
+  spdx.startsWith('CC0') ? '/src/images/cc-zero.svg' : null
+)
+</script>
+
+<template>
+  <div v-if="spdx">
+    <img v-if="licenseImage" class="license" :src="licenseImage" :alt="spdx">
+    <span v-else>⚖️ {{ spdx }}</span>
+  </div>
+</template>
+
+<style scoped>
+img.license {
+    width: 88px;
+}
+</style>
