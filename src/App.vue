930701cd82c174b1d18fa8c95a2e97b72a7e3b4f
Lukas Kluft <lukas.kluft@gmail.com>
2025-02-03 21:47:34 +0100

    Use ORCESTRA logo in navbar
    Co-authored-by: Tobias Kölling <tobias.koelling@mpimet.mpg.de>

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
-import IPFSStatus from './components/IPFSStatus.vue'
+import IPFSStatus from './components/IPFSStatus.vue';
+import OrcestraLogo from './images/orcestra-logo.svg';
 
 const ds_url = ref("http://127.0.0.1:8080/ipns/latest.orcestra-campaign.org/products/HALO/dropsondes/Level_3/PERCUSION_Level_3.zarr")
 </script>
 
 <template>
   <nav>
-    <div class="brand"><a href="#/">ORCESTRA Data Browser</a></div>
+    <div class="brand"><a href="#/"><OrcestraLogo height="28px" viewBox="0 0 558.77 63"/> Data Browser</a></div>
     <IPFSStatus class="_end" />
   </nav>
   <RouterView />
