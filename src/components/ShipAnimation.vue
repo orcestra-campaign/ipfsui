b37d4b2890d031023e55ef905b1ed96cb4186c2b
Lukas Kluft <lukas.kluft@gmail.com>
2025-02-07 14:53:05 +0100

    Add an animation of a moving ship

---
+++ src/components/ShipAnimation.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="loading-container">
+    <div class="island cape-verde float">🏝️</div>
+    <div class="ship float">🛥️</div>
+    <div class="island barbados float">🏝️</div>
+  </div>
+</template>
+
+<style scoped>
+
+.loading-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.float {
+  font-size: 2.6em;
+  position: absolute;
+  display: inline;
+}
+
+.barbados {
+  transform: translate(-15vw);
+}
+
+.cape-verde {
+  transform: translate(15vw);
+}
+
+.ship {
+  animation: move 32s linear infinite;
+}
+
+@keyframes move {
+    0% { transform: translate(15vw); }
+    100% { transform: translate(-15vw); }
+}
+</style>
