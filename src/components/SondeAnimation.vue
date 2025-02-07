14a034c01a2ebf993692a530501b5651b4137262
Lukas Kluft <lukas.kluft@gmail.com>
2025-02-07 15:03:32 +0100

    Add an animation of an ascending radiosonde

---
+++ src/components/SondeAnimation.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="loading-container">
+    <div class="island float">🏝️</div>
+    <div class="balloon float">🎈</div>
+  </div>
+</template>
+
+<style scoped>
+.loading-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.float {
+  font-size: 2.6em;
+  position: absolute;
+}
+
+.balloon {
+  animation: move 16s linear infinite;
+}
+
+@keyframes move {
+  0% { transform: translate(0, 0); }
+  100% { transform: translate(-5vw, -25vh); }
+}
+</style>
