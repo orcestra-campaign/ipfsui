044793715158d5160db1a3344de8668b163d6cdc
Lukas Kluft <lukas.kluft@gmail.com>
2025-02-13 15:12:39 +0100

    Close border around IPFS CID box while loading

--- src/components/PathView.vue
+++ src/components/PathView.vue
@@ -17,16 +17,18 @@ const {src, item_cid} = defineProps<{ src: string, item_cid?: CID }>();
 <style scoped>
 .pathView {
     border-style: solid;
-    border-top-width: 2px;
-    border-left-width: 2px;
-    border-right-width: 2px;
-    border-bottom-width: 0;
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
+    border-width: 2px;
+    border-radius: 5px;
     border-color: var(--highlight-bg-color);
     padding: 3px 5px;
     overflow: hidden;
 }
+
+.pathView:has(+ .head) {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+}
+
 div.srcPath {
     font-weight: bold;
 }
