0b72ea8549b8ca93c14e586d47094d0d8a2f4e26
Lukas Kluft <lukas.kluft@gmail.com>
2025-09-17 11:53:06 +0200

    Add navbar div to `NotFound` component

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -3,7 +3,9 @@ import Nav from './Nav.vue';
 </script>
 
 <template>
-    <Nav />
+    <div class="navbar">
+        <Nav />
+    </div>
     <h1>Page Not Found</h1>
     If you intended to inspect a dataset, try navigating to <code>#/ds/&lt;your dataset url&gt;</code>
 </template>
