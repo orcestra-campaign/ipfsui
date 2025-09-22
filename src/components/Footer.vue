bf56300076edd26d4675a448458678ba914788ff
Lukas Kluft <lukas.kluft@gmail.com>
2025-09-22 21:51:38 +0200

    Protect clear space around MPG Minerva

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,7 +7,7 @@ import MaxPlanckLogo from '../images/mpg-minerva.svg';
         <div class="footer-content">
             <div class="left"></div>
             <div class="center"></div>
-            <div class="right"><MaxPlanckLogo height="72px" viewBox="0 0 113.5 113.4"/></div>
+            <div class="right"><MaxPlanckLogo class="logo"/></div>
         </div>
     </div>
 </template>
@@ -22,10 +22,14 @@ import MaxPlanckLogo from '../images/mpg-minerva.svg';
 .footer-content {
   max-width: 1280px;
   margin: 0 auto;
-  padding: 10px 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.logo {
+  margin: 12px;
+  height: 72px;
 }
 
 .left {
