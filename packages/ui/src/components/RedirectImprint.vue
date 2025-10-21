6a20782e1aa289471941a17217b3546ff17b66c2
Lukas Kluft <lukas.kluft@gmail.com>
2025-10-21 10:41:19 +0200

    Add component to redirect to MPI-M imprint

---
+++ packages/ui/src/components/RedirectImprint.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Redirecting to Max Planck Institute for Meteorology imprint...</div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  window.location.replace("https://www.mpimet.mpg.de/en/imprint");
+});
+</script>
