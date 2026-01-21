11d7e4629f80c0c8f4f2cb3d05b3fe8a9776d3b4
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2026-01-21 22:23:03 +0100

    add 'dito stac write-catalog'

--- packages/cli/src/commands/stac.ts
+++ packages/cli/src/commands/stac.ts
@@ -1,9 +1,13 @@
 import { Command } from "commander";
 import { withHelia } from "../configureHelia.js";
 import cid2stac from "../cid2stac.js";
+import { StacItem } from "@orcestra/utils";
+
+import * as fs from "node:fs/promises";
 
 export default function makeStacCommand(stacCommand: Command) {
   stacCommand.description("STAC handling");
+    
   stacCommand.command("get-meta")
   .description("get STAC metadata")
   .requiredOption('--cid <CID>', 'dataset CID')
@@ -12,5 +16,38 @@ export default function makeStacCommand(stacCommand: Command) {
       const stac = await cid2stac(options.cid, helia);
       process.stdout.write(JSON.stringify(stac));
     })
+  })
+    
+  stacCommand.command("write-catalog")
+  .description("write STAC catalog from products.json")
+  .requiredOption('-i --infile <products.json>', 'products.json (e.g. from index scan) ')
+  .requiredOption('-o --outfolder <folder>', 'output folder for generated stac catalog')
+  .action(async (options: {infile: string, outfolder: string}) => {
+      const items = JSON.parse(await fs.readFile(options.infile, {encoding: "utf-8"})) as StacItem[];
+
+      const id2path = (id: string) => "items/" + id + ".json";
+
+      const catalog = {
+        type: "Catalog",
+        stac_version: "1.1.0",
+        stac_extensions: [],
+        id: "orcestra_products",
+        title: "ORCESTRA products catalog",
+        description: "Catalog for ORCESTRA campaign data",
+        links: items.map((item) => {
+          return {
+            href: id2path(item.id),
+            rel: "item",
+            type: "application/geo+json",
+            title: item.properties?.title,
+          };
+        }),
+      };
+
+      await fs.mkdir(options.outfolder + "/items", { recursive: true });
+      await fs.writeFile(options.outfolder + "/catalog.json", JSON.stringify(catalog), {encoding: "utf-8"});
+      for (const item of items) {
+        await fs.writeFile(options.outfolder + "/" + id2path(item.id), JSON.stringify(item), {encoding: "utf-8"});
+      }
   })
 }
