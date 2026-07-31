ca046ffc6c865d1d598890d01945c43fa9ea11af
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2026-07-31 09:55:15 +0200

    increase stac test runtime

--- packages/cli/tests/stac.test.ts
+++ packages/cli/tests/stac.test.ts
@@ -5,4 +5,4 @@ import { withHelia, default as configureHelia } from "../src/configureHelia.ts";
 test("stac extracts title", async () => await withHelia(async (helia) => {
   const stac = await cid2stac("bafybeiesyutuduzqwvu4ydn7ktihjljicywxeth6wtgd5zi4ynxzqngx4m", helia);
   expect(stac.properties.title).toContain('BEACH dropsonde dataset (Level 3)');
-}), 10000);
+}), 60000);
